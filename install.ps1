1a70f8baf05ce5cb42f109f82e79f2c3f4aade10
Fr3akPr0gramm3r <124489351+Fr3akPr0gramm3r@users.noreply.github.com>
2023-03-04 15:53:53 +0000

    Create install.ps1

---
+++ install.ps1
@@ -0,0 +1,12 @@
+& "Set-ExecutionPolicy RemoteSigned -Scope CurrentUser"
+& "irm get.scoop.sh | iex"
+
+& "scoop install git"
+& "scoop bucket add extras"
+& "scoop bucket add main"
+& "scoop bucket add nonportable"
+& "scoop bucket add versions"
+& "scoop bucket add java"
+& "scoop bucket add nerd-fonts"
+
+& "scoop install codeblocks tcc qt-creator v-weekly go rust nodejs deno python dmd ldc clangd dotnet-sdk qbittorrent vscode vscodium sublime-text keepassxc postman conemu  curl wget  bat   brave openjdk eclipse-android eclipse-platform eclipse-java eclipse-php eclipse-javascript eclipse-rust eclipse-cpp eclipse-embedcpp goneovim neovim micro ntop liteide notepadplusplus notepad2-mod lapce audacity etcher rufus julia openssh mc tor-browser freetube openvpn goodbyedpi vcpkg conan  protonvpn-np php composer lantern-client doom-retro gzdoom netbeans vcredist2008 vcredist2010 vcredist2012 vcredist2013 vcredist2015 vcredist2017 vcredist2019 vcredist2022 touch lsd time ls sudo kdenlive sbcl haskell"
