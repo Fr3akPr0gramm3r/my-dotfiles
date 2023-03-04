e8c29eb3ae674a6fd0ef949cc24a43f4cda43410
FFFReak <124489351+Fr3akPr0gramm3r@users.noreply.github.com>
2023-03-04 17:23:14 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+
 os_info="`cat /proc/version`"
 
 if [[ $os_info == *"Debian"* ]]; then 
 	sh ./src/install_on_debian.sh
-elif [[ $os_info == *"Arch"* ]]
+elif [[ $os_info == *"Arch"* ]]; then
 	sh ./src/install_on_arch.sh
 fi
