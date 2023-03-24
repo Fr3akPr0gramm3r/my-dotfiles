370974bb5c7ae39b1d34590ad7c8b313bebc7485
ZueffC <zueffc@gmail.com>
2023-03-24 16:49:09 +0300

    Added some new packages for install

--- src/install_on_arch.sh
+++ src/install_on_arch.sh
@@ -8,7 +8,7 @@ sudo pacman -S zsh wget
 sudo pacman -S micro neovim htop alacritty nvtop
 sudo pacman -Rncs grml-zsh-config
 sudo pacman -S openssl-1.1
-sudo pacman -S chromium qbittorrent lmms ardour ark  gnome-boxes audacity kolourpaint keepassxc
-sudo pacman -S nim dub 
+sudo pacman -S chromium qbittorrent lmms ardour ark gnome-boxes audacity kolourpaint keepassxc
+sudo pacman -S nim dub dotnet-sdk go tcc cmake  
 sudo pacman -S qtcreator code kdevelop
 sudo pacman -S discord telegram-desktop
