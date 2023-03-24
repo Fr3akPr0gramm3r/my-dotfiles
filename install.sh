5cd934592e11768b34f533e11adb07395ddb4c3f
ZueffC <zueffc@gmail.com>
2023-03-24 15:55:45 +0300

    Added descriptions for some doings added new installer for arch-based systems

--- install.sh
+++ install.sh
@@ -8,15 +8,22 @@ elif [[ $os_info == *"Arch"* ]]; then
 	sudo ./src/install_on_arch.sh
 fi
 
-
+#oh-my-zsh installation
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+#rustup installer
 sudo curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+#modern js/ts runtime installer
 sudo curl -fsSL https://deno.land/x/install/install.sh | sh
+
+#themes for rofi with installation
 git clone https://github.com/lr-tech/rofi-themes-collection.git themes-rofi
-sudo mkdir -p ~/.local/share/rofi/themes/
-sudo cp themes-rofi/themes/* ~/.local/share/rofi/themes/
+sudo mkdir -p ~/.config/rofi/themes/
+sudo cp themes-rofi/themes/* ~/.config/rofi/themes/
 rofi-theme-selector
 
+#GoPlus installer
 git clone https://github.com/goplus/gop.git
 cd gop
 ./all.bash
--- src/install_on_arch.sh
+++ src/install_on_arch.sh
@@ -1,2 +1,5 @@
 #!/bin/bash 
+sudo pacman -Syyu
 sudo pacman -S archlinux-keyring 
+
+sudo pacman -S micro neovim
